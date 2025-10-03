bbc2ae50e6bd82e3d76b6a76087866b339c13e9b
mateus-x17 <mateusinaciorocha005@gmail.com>
2025-10-03 06:32:38 -0300

    esboço estilos de bg

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -9,14 +9,14 @@ const kpis = [
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="w-full h-full p-6 bg-gray-200 dark:bg-gray-700">
       <h1 className="text-2xl font-bold mb-6">Dashboard</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {kpis.map((kpi) => (
           <div
             key={kpi.title}
-            className="bg-white dark:bg-gray-700 shadow rounded-lg p-6 flex flex-col justify-center items-start transition-transform transform hover:scale-105"
+            className="bg-gradient-light dark:bg-gradient-dark shadow rounded-lg p-6 flex flex-col justify-center items-start transition-transform transform hover:scale-105"
           >
             <p className="text-gray-500 dark:text-gray-300">{kpi.title}</p>
             <h2 className="text-3xl font-bold text-gray-900 dark:text-white">{kpi.value}</h2>
