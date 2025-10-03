d44182b3c9f2dcb8bc5b48340b04185d05be0cab
mateus-x17 <mateusinaciorocha005@gmail.com>
2025-10-03 03:22:00 -0300

    adc class dark

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: 'class', // habilita dark mode via classe 'dark' no elemento raiz
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
