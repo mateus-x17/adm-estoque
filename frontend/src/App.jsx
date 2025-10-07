dde7a9f56d9baa545444a9fb45ed80aef850443d
mateus <mateusinaciorocha005@gmail.com>
2025-10-06 21:38:41 -0300

    remove rotas antigas adc no main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./layout/Layout";
-import Dashboard from "./pages/Dashboard";
-import Produtos from "./pages/Produtos";
+import Home from "./pages/Home.jsx";
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/produtos" element={<Produtos />} />
-          {/* Outras páginas podem ser adicionadas aqui */}
-        </Routes>
-      </Layout>
-    </Router>
+    <>
+      <Home />
+    
+    </>
   );
 }
 
 export default App;
-
