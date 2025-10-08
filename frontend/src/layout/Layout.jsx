0186e1ebbfdf43f56d1a97f9cd928934b93203aa
mateus <mateusinaciorocha005@gmail.com>
2025-10-08 19:54:41 -0300

    upd responsividade para sidebar

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -21,17 +21,21 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="h-screen flex md:flex-row flex-col">
-      <div className="hidden md:block">
-        <Sidebar closeSidebar={() => {}} /> {/* closeSidebar para fechar a sidebar */}
-      </div>
-      <div className="flex flex-col flex-1">
-        <Header /> {/* flex-1 aqui faz a coluna ocupar todo o espaço */}
-        <main className="w-full flex-1 overflow-y-auto bg-gray-200">
-          <Outlet /> {/* Outlet para renderizar as rotas filhas */}
+    <div className="h-screen flex md:flex-row flex-col overflow-hidden">
+      {/* Sidebar fixa no desktop */}
+      <aside className="hidden md:flex h-full">
+        <Sidebar closeSidebar={() => {}} />
+      </aside>
+
+      {/* Conteúdo principal */}
+      <div className="flex flex-col flex-1 h-full">
+        <Header />
+        <main className="flex-1 overflow-y-auto bg-gray-200">
+          <Outlet />
         </main>
       </div>
     </div>
   );
 };
+
 export default Layout;
