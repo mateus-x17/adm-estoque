5c15f16687f980328f6a1e46b817b04e6e1f93ae
mateus <mateusinaciorocha005@gmail.com>
2025-10-09 03:59:09 -0300

    upd verificação de user  e redirecionamento

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,45 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useUserStore } from "../store/userStore";
 
-// const Layout = ({ children }) => {
-//   return (
-//     <div className="h-screen flex md:flex-row flex-col">
-//       <div className="hidden md:block">
-//         <Sidebar />
-//       </div>
-//       <div className="flex flex-col flex-1">
-//         <Header /> {/* flex-1 aqui faz a coluna ocupar todo o espaço */}
-//         <main className="w-full flex-1 overflow-y-auto bg-gray-200">
-//           {children}
-//         </main>
-//       </div>
-//     </div>
-//   );
-// };
-
 const Layout = () => {
-  // verificar se o store userStore tem dados do usuário
-  // se não tiver, redirecionar para a página de login antes de renderizar o layout com o dashboard
-    const {user } = useUserStore();
-    if (!user || !user.nome) {
-      // Se não houver dados do usuário, redirecionar para a página de login
-      alert("Por favor, efetue o login primeiro.");
-      window.location.replace("http://localhost:5173/auth");
-      return null; // Ou um componente de carregamento, se preferir
-    }
+  const navigate = useNavigate();
+  const { user } = useUserStore();
 
+  // Verifique se o usuário estiver logado - usamos o useEffect para redirecionar se não estiver
+  // o useEffect depende de 'user' e 'navigate'
+  // evita erros de renderização condicional
+  useEffect(() => {
+    if (!user || !user.nome) {
+      alert("Por favor, efetue o login primeiro.");
+      navigate("/auth");
+    }
+  }, [user, navigate]);
+
+  // Enquanto o usuário não estiver definido, pode retornar null ou um loader
+  if (!user || !user.nome) {
+    return null;
+  }
 
   return (
     <div className="h-screen flex md:flex-row flex-col overflow-hidden">
-      {/* Sidebar fixa no desktop */}
       <aside className="hidden md:flex h-full">
         <Sidebar closeSidebar={() => {}} />
       </aside>
 
-      {/* Conteúdo principal */}
       <div className="flex flex-col flex-1 h-full">
         <Header />
         <main className="flex-1 overflow-y-auto bg-gray-200">
