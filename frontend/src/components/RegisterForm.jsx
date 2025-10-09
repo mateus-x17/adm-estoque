0753cf028a62870c939507201661d471a94f9f49
mateus <mateusinaciorocha005@gmail.com>
2025-10-09 04:43:33 -0300

    adc fetch para registro no backend

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,21 +1,64 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserStore } from "../store/userStore";
 
 const RegisterForm = () => {
+  const url = "http://localhost:5000/auth/register";
+  const { setUser } = useUserStore();
+
+  const navigate = useNavigate();
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    const dados = { nome, email, senha };
+    console.log("dados do form: ", dados);
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dados),
+      });
+      const data = await response.json();
+      console.log("resposta do registro: ", data);
+      if (response.ok) {
+        // login bem-sucedido - salvar token e dados no storage e redirecionar para dashboard
+        // setUser(data.usuario.user, data.usuario.token);
+        alert("registro de usuario bem-sucedido: faça login", data.message);
+      }
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+    }
+  };
+
+
   return (
-    <form className="w-full max-w-sm flex flex-col gap-4 animate-slideUp">
+    <form className="w-full max-w-sm flex flex-col gap-4 animate-slideUp"
+      onSubmit={submitForm}>
       <input
         type="text"
+        onChange={(e) => setNome(e.target.value)}
+        required
         placeholder="Nome completo"
         className="p-3 rounded-md border border-gray-300 dark:border-gray-600 bg-transparent focus:ring-2 focus:ring-indigo-500 outline-none"
       />
       <input
         type="email"
         placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        required
         className="p-3 rounded-md border border-gray-300 dark:border-gray-600 bg-transparent focus:ring-2 focus:ring-indigo-500 outline-none"
       />
       <input
         type="password"
         placeholder="Senha"
+        onChange={(e) => setSenha(e.target.value)}
+        required
         className="p-3 rounded-md border border-gray-300 dark:border-gray-600 bg-transparent focus:ring-2 focus:ring-indigo-500 outline-none"
       />
       <button
