42d40b4d3fccdb1f2e50f615f6801c6d9ee7bdf3
mateus <mateusinaciorocha005@gmail.com>
2025-10-09 03:59:52 -0300

    adc estado para dados de usuario

---
+++ frontend/src/store/userStore.js
@@ -0,0 +1,8 @@
+import { create } from "zustand";
+
+export const useUserStore = create((set) => ({
+  user: null,
+  token: null,
+  setUser: (user, token) => set({ user, token }),
+  clearUser: () => set({ user: null, token: null }),
+}));
