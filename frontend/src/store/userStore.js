20531a0e712bb9007d0b5f34e4a747f41af2e8a2
mateus <mateusinaciorocha005@gmail.com>
2025-10-10 03:05:12 -0300

    explicação

--- frontend/src/store/userStore.js
+++ frontend/src/store/userStore.js
@@ -6,3 +6,12 @@ export const useUserStore = create((set) => ({
   setUser: (user, token) => set({ user, token }),
   clearUser: () => set({ user: null, token: null }),
 }));
+
+// resposta do login:  
+// token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzU5OTkzMjAyLCJleHAiOjE3NjAwMjIwMDJ9.TXkm5vhKsvVLmCVh1ulZ5LukKLtT-4wWLJ0ZNCVCe84"
+// user:{ email: "admin@empresa.com", id: 1, nome: "Admin Inicial", role: "ADMIN" }
+
+
+// modal com menssagem recebida da api de sucesso/erro 
+// enviar a mensagem como prop 
+// enviar o tipo de mensagem (sucesso/erro) como prop para controlar o estilo do modal (if sucess estilo verde else estilo vermelho)
