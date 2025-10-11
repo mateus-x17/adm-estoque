4a8e4a3c0542311092b54f96863f33fc62df443d
mateus-x17 <mateusinaciorocha005@gmail.com>
2025-10-11 09:28:31 -0300

    adc func para chamada de API e Modal de mensagem

--- frontend/src/pages/Usuarios.jsx
+++ frontend/src/pages/Usuarios.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { FaUser } from "react-icons/fa";
-import {usuarios} from '../dados.js'
+// import {usuarios} from '../dados.js'
+import { useUserStore } from "../store/userStore";
+import { useThemeStore } from "../store/useThemeStore.js";
+import ModalMensagem from "../components/ModalMensagem.jsx";
 
 const getRoleColor = (role) => {
   switch (role) {
@@ -22,9 +25,54 @@ const getInitials = (name) => {
 };
 
 const Usuarios = () => {
+  const url = "http://localhost:5000/users";
+  const [usuarios, setUsuarios] = useState([]);
+  const { token } = useUserStore();
+
+  // tema dark/light
+  const { darkMode } = useThemeStore();
+  useEffect(() => {
+    if (darkMode) document.documentElement.classList.add("dark");
+    else document.documentElement.classList.remove("dark");
+  }, [darkMode]);
+
+  // ESTADO DO MODAL - mensagem e tipo (sucesso/erro)
+  const [modal, setModal] = useState({ visible: false, mensagem: "", tipo: "" });
+
+  // função para carregar usuários da API
+  const carregarUsuarios = async () => {
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setUsuarios(data);
+      } else {
+        console.error("Erro ao carregar usuarios:", data.error);
+        // Atualiza estado para exibir modal
+        setModal({ visible: true, mensagem: data.error, tipo: "erro" });
+      }
+    } catch (error) {
+      console.error("Erro ao carregar usuarios:", error);
+      setModal({ visible: true, mensagem: "Erro de conexão com o servidor", tipo: "erro" });
+    }
+  };
+
+  // carrega usuários ao montar o componente toda vez que a página for aberta
+  useEffect(() => {
+    carregarUsuarios();
+  }, []);
+
   return (
     <div className="w-full min-h-screen bg-gray-100 dark:bg-gray-900 pb-6 pt-2 px-6 transition-all duration-500">
-      {/* Banner */}
+    {/* Banner */}
     {/* <section className="relative w-full h-[20%] p-3 flex flex-col items-center justify-center text-center text-white overflow-hidden bg-gradient-to-r from-purple-800 via-blue-400 to-green-600 bg-[length:400%_400%] animate-gradientShift mb-12 rounded-xl">
         <div className="absolute inset-0 bg-black/30" />
         <h1 className="relative z-10 text-4xl md:text-5xl font-bold drop-shadow-lg animate-fadeInDown">
@@ -44,6 +92,11 @@ const Usuarios = () => {
           Visualize, edite e gerencie todos os usuários cadastrados no sistema.
         </p>
       </header>
+
+      {/* Renderiza modal apenas se visible = true * - em caso de erro */}
+      {modal.visible && (
+        <ModalMensagem mensagem={modal.mensagem} tipo={modal.tipo} onClose={() => setModal({ visible: false })} />
+      )}
 
       {/* Grid de usuários */}
       <div className="max-w-6xl mx-auto grid gap-8 sm:grid-cols-2 lg:grid-cols-3 pt-6 ">
@@ -94,10 +147,12 @@ const Usuarios = () => {
 
                 {/* Datas */}
                 <p className="mt-2 text-xs">
-                    Criado: {user.createdAt.toLocaleDateString()}
+                    {/* Criado: {user.createdAt.toLocaleDateString()} */}
+                    Criado: {new Date(user.createdAt).toLocaleDateString()}
                 </p>
                 <p className="text-xs">
-                    Atualizado: {user.updatedAt.toLocaleDateString()}
+                    {/* Atualizado: {user.updatedAt.toLocaleDateString()} */}
+                    Atualizado: {new Date(user.updatedAt).toLocaleDateString()}
                 </p>
 
                 {/* Botões de ação */}
