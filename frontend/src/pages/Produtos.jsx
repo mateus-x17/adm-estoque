51a7e2ffbb85e30595c4c6dbf233c4e60b79b73f
mateus-x17 <mateusinaciorocha005@gmail.com>
2025-10-13 00:05:33 -0300

    adc mensagem de load na tabela

--- frontend/src/pages/Produtos.jsx
+++ frontend/src/pages/Produtos.jsx
@@ -58,7 +58,16 @@ const Produtos = () => {
         </p>
       </header>
 
-      {loaded && <TabelaProdutos produtos={produtos} abrirModal={abrirModal} />}
+      {
+        loaded ? (
+          <TabelaProdutos produtos={produtos} abrirModal={abrirModal} />
+        ) : (
+          <div className="flex-grow flex items-center justify-center">
+            <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-16 w-16">
+            </div>
+          </div>
+        )
+      }
 
       {produtoSelecionado && (
         <ModalProduto
