491f2e3aa654f63b984c31dbfeceaaedf0ff30a7
mateus <mateusinaciorocha005@gmail.com>
2025-10-18 02:31:48 -0300

    upd renderização de imagem condicional

--- frontend/src/components/ProdutoRow.jsx
+++ frontend/src/components/ProdutoRow.jsx
@@ -8,7 +8,13 @@ const ProdutoRow = ({ produto, index, abrirModal }) => {
     >
       <td className="px-4 py-3">
         <img
-          src={produto.imagem && produto.imagem.startsWith("/uploads") ? produto.imagem : "/default.png"}
+          src={
+            produto.imagem
+              ? `http://localhost:5000${
+                  produto.imagem.startsWith("/") ? "" : "/"
+                }${produto.imagem}`
+              : "/default.png"
+          }
           alt={produto.nome}
           className="w-14 h-14 object-cover rounded-md"
         />
