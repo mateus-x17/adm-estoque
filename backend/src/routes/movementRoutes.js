d9f2d48ce137ecef25774e560d0f2107655029f8
mateus <mateusinaciorocha005@gmail.com>
2025-10-18 04:36:56 -0300

    upd permissão a listagem de movimentações

--- backend/src/routes/movementRoutes.js
+++ backend/src/routes/movementRoutes.js
@@ -6,5 +6,5 @@ import { listMovements } from "../controllers/movementController.js";
 const router = express.Router();
 router.use(authMiddleware); //todas rotas usam esse middleware de autenticacao
 
-router.get("/", permit("ADMIN","GERENTE"), listMovements); // histórico (só gerentes/admin) usa o middleware permit que verifica o role
+router.get("/", permit("ADMIN","GERENTE", "OPERADOR"), listMovements); // histórico (só gerentes/admin) usa o middleware permit que verifica o role
 export default router;
