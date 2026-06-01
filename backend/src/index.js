21e4ad66901ee618c6cabe2fc081c1cc9aada464
mateus-x17 <mateusinaciorocha005@gmail.com>
2026-06-01 03:47:56 -0300

    upd cors para permitir envio de arquivos do frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,13 @@ if (!process.env.JWT_SECRET || process.env.JWT_SECRET.length < 16) {
 const app = express();
 const PORT = process.env.PORT || 5000;
 // middlewares
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: {
+      policy: "cross-origin",
+    },
+  })
+); //permitir requisição do endpoint de frontend
 app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(requestLogger);
